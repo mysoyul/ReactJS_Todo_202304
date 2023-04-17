673853af4a8b1aaf3535a33ccfad07f34a3a140e
mysoyul <vega2k_@naver.com>
2023-04-17 17:31:58 +0900

    todo app add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Form from './components/Form';
+import TodoItemList from './components/TodoItemList';
+import TodoListTemplate from './components/TodoListTemplate';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  id = 3; // 이미 0,1,2 가 존재하므로 3으로 설정
+  //상태변수를 포함하고 있는 state 객체선언
+  state = {
+    todo: '',
+    todos: [
+      { id: 0, text: '리액트 소개', checked: false },
+      { id: 1, text: '리액트 구조', checked: true },
+      { id: 2, text: '리액트 사용', checked: false }
+    ]
+  };
+  //Event Handler 함수선언
+  handleChange = (e) => {
+    this.setState({
+      todo: e.target.value // todo 의 다음 바뀔 값
+    });
+  };
+  handleCreate = () => {
+    const { todo, todos } = this.state;
+    const newTodo = {
+      id: this.id++,
+      text: todo,
+      checked: false
+    };
+    this.setState({
+      // concat 을 사용하여 배열에 추가
+      todos: [...todos, newTodo],
+      todo: '', // todo 초기화
+    });
+  }
+  handleKeyPress = (e) => {
+    // 눌려진 키가 Enter 이면 handleCreate 호출
+    if (e.key === 'Enter') {
+      this.handleCreate();
+    }
+  }
+
+  render() {
+    return (
+      <TodoListTemplate form={<Form />}>
+        <TodoItemList />
+      </TodoListTemplate>
+    );
+  }
 }
-
 export default App;
