7d5c6a9b2b661c4da6785b5cfa3e70d2679e5963
mysoyul <vega2k_@naver.com>
2023-04-18 16:06:25 +0900

    App.js update

--- src/App.js
+++ src/App.js
@@ -4,25 +4,11 @@ import TodoItemList from './components/TodoItemList';
 import TodoListTemplate from './components/TodoListTemplate';
 
 class App extends Component {
-  id = 3; // 이미 0,1,2 가 존재하므로 3으로 설정
-  //상태변수를 포함하고 있는 state 객체선언
-  state = {
-    todos: []
-  };
-
-  handleToggle = (id) => {
-    const { todos } = this.state;
-    this.setState({
-      todos: todos.map(todo => todo.id === id ? { ...todo, checked: !todo.checked } : todo)
-    });
-  }
   
   render() {
-    const { handleToggle } = this;
-
     return (
       <TodoListTemplate form={<Form />}>
-        <TodoItemList  myToggle={handleToggle} />
+        <TodoItemList  />
       </TodoListTemplate>
     );
   }
