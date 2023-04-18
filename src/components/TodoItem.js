c895290c90a8666c04778f922674838ed0e43ff6
mysoyul <vega2k_@naver.com>
2023-04-18 16:01:16 +0900

    TodoItem.js update

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 import './TodoItem.css';
+import { connect } from 'react-redux';
+import { removeTodo } from '../actions';
 class TodoItem extends Component {
     shouldComponentUpdate(nextProps, nextState) {
         return this.props.checked !== nextProps.checked;
     }
 
+    handleRemove = (id) => {
+        this.props.removeTodo(id);
+    }
+
     render() {
         const { id, text, checked, onToggle } = this.props;
+        const { handleRemove } = this;
+
         return (
             <div className="todo-item" onClick={() => onToggle(id)}>
                 <div className="remove" onClick={(e) => {
                     e.stopPropagation(); // onToggle 이 실행되지 않도록 함
-                    //onRemove(id)
+                    handleRemove(id)
                 }
                 }>&times;</div>
                 <div className={`todo-text ${checked && 'checked'}`}>
