587bf2271787137e9351fa59f831fede5ccea4b7
mysoyul <vega2k_@naver.com>
2023-04-18 16:07:04 +0900

    App.js update

--- src/components/TodoItemList.js
+++ src/components/TodoItemList.js
@@ -16,14 +16,13 @@ class TodoItemList extends Component {
     }
 
     render() {
-        const { todos, myToggle } = this.props;
+        const { todos } = this.props;
         const todoList = todos.map(
             ({ id, text, checked }) => (
                 <TodoItem
                     id={id}
                     text={text}
                     checked={checked}
-                    onToggle={myToggle}
                     key={id}
                 />
             )
