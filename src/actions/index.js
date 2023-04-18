4356cb07a78fe3f1f2b5786465338e7244de224a
mysoyul <vega2k_@naver.com>
2023-04-18 15:10:31 +0900

    actions update

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,3 +5,18 @@ export const FETCH_TODOS = "FETCH_TODOS";
 
 const apiUrl = 'http://localhost:4500/api/todos';
 
+export const fetchAllTodos = () => {
+    return (dispatch) => {
+        axios.get(apiUrl) //Promise
+            .then(res => {
+                dispatch({
+                    type: FETCH_TODOS,
+                    payload: res.data
+                })
+            })
+            .catch(error => {
+                console.error(error);
+                throw(error);
+            });
+    }
+};
