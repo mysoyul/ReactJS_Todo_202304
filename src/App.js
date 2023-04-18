101c1eb10e17208bcae6fbfe39ff2a7840ec9409
mysoyul <vega2k_@naver.com>
2023-04-18 15:36:16 +0900

    form.js update

--- src/App.js
+++ src/App.js
@@ -7,34 +7,9 @@ class App extends Component {
   id = 3; // 이미 0,1,2 가 존재하므로 3으로 설정
   //상태변수를 포함하고 있는 state 객체선언
   state = {
-    todo: '',
     todos: []
   };
-  //Event Handler 함수선언
-  handleChange = (e) => {
-    this.setState({
-      todo: e.target.value // todo 의 다음 바뀔 값
-    });
-  };
-  handleCreate = () => {
-    const { todo, todos } = this.state;
-    const newTodo = {
-      id: this.id++,
-      text: todo,
-      checked: false
-    };
-    this.setState({
-      // ...Spread operator 사용하여 배열에 추가
-      todos: [...todos, newTodo],
-      todo: '', // todo 초기화
-    });
-  }
-  handleEnter = (e) => {
-    // 눌려진 키가 Enter 이면 handleCreate 호출
-    if (e.keyCode === 13) {
-      this.handleCreate();
-    }
-  }
+
   handleToggle = (id) => {
     const { todos } = this.state;
     this.setState({
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,6 +2,35 @@ import React, { Component } from 'react';
 import './Form.css';
 
 class Form extends Component {
+    state = {
+        todo: ''
+    };
+    //Event Handler 함수선언
+    handleChange = (e) => {
+        this.setState({
+            todo: e.target.value // todo 의 다음 바뀔 값
+        });
+    };
+    handleCreate = () => {
+        const { todo, todos } = this.state;
+        const newTodo = {
+            id: this.id++,
+            text: todo,
+            checked: false
+        };
+        this.setState({
+            // ...Spread operator 사용하여 배열에 추가
+            todos: [...todos, newTodo],
+            todo: '', // todo 초기화
+        });
+    }
+    handleEnter = (e) => {
+        // 눌려진 키가 Enter 이면 handleCreate 호출
+        if (e.keyCode === 13) {
+            this.handleCreate();
+        }
+    }
+
     render() {
         const { todo, myEnter, myChange, myCreate } = this.props;
         return (
