a258e716dd7f596965f04fd0a1cdc2b41ac326f0
mysoyul <vega2k_@naver.com>
2023-04-18 16:04:50 +0900

    ToggleTodo action add

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,22 +6,7 @@ const apiUrl = 'http://localhost:4500/api/todos';
 export const FETCH_TODOS = "FETCH_TODOS";
 export const ADD_TODO = "ADD_TODO";
 export const REMOVE_TODO = "REMOVE_TODO";
-
-export const removeTodo = (id) => {
-    return (dispatch) => {
-        axios.delete(`${apiUrl}/${id}`)
-            .then(res => {
-                dispatch({
-                    type: REMOVE_TODO,
-                    payload: res.data
-                })
-            })
-            .catch(error => {
-                console.log(error);
-                throw (error);
-            })
-    }
-};
+export const TOGGLE_TODO = "TOGGLE_TODO";
 
 export const fetchAllTodos = () => {
     return (dispatch) => {
@@ -54,3 +39,34 @@ export const addTodo = (todo) => {
             })
     }
 };
+export const removeTodo = (id) => {
+    return (dispatch) => {
+        axios.delete(`${apiUrl}/${id}`)
+            .then(res => {
+                dispatch({
+                    type: REMOVE_TODO,
+                    payload: res.data
+                })
+            })
+            .catch(error => {
+                console.log(error);
+                throw (error);
+            })
+    }
+};
+
+export const toggleTodo = (todo) => {
+    return (dispatch) => {
+        axios.patch(`${apiUrl}/${todo.id}`, todo)
+            .then(res => {
+                dispatch({
+                    type: TOGGLE_TODO,
+                    payload: res.data
+                })
+            })
+            .catch(error => {
+                console.log(error);
+                throw (error);
+            })
+    }
+}
