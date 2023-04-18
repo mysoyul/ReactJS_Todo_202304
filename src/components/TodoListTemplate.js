9ca3b0d93f976099f03c5ea056c85aad3a2f6335
mysoyul <vega2k_@naver.com>
2023-04-18 16:21:30 +0900

    process env

--- src/components/TodoListTemplate.js
+++ src/components/TodoListTemplate.js
@@ -5,7 +5,7 @@ const TodoListTemplate = ({ form, children }) => {
     return (
         <main className="todo-list-template">
             <div className="title">
-                오늘의 할 일
+                오늘의 할 일 ({process.env.REACT_APP_TITLE})
             </div>
             <section className="form-wrapper">
                 {form}
