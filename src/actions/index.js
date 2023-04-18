251943ffb10be502ada84f3b5863693d17ac5732
mysoyul <vega2k_@naver.com>
2023-04-18 15:31:19 +0900

    addTodo action add

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 //Action type 정의
 export const FETCH_TODOS = "FETCH_TODOS";
+export const ADD_TODO = "ADD_TODO";
 
 const apiUrl = 'http://localhost:4500/api/todos';
 
@@ -16,7 +17,23 @@ export const fetchAllTodos = () => {
             })
             .catch(error => {
                 console.error(error);
-                throw(error);
+                throw (error);
             });
+    }
+};
+
+export const addTodo = (todo) => {
+    return (dispatch) => {
+        axios.post(apiUrl, todo)
+            .then(res => {
+                dispatch({
+                    type: ADD_TODO,
+                    payload: res.data
+                })
+            })
+            .catch(error => {
+                console.log(error);
+                throw (error);
+            })
     }
 };
